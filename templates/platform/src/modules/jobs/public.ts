c926bbe33896f105656338c22470d50dee6ff8ed
HAKPIN LEE <hakbin9984@gmail.com>
2026-08-07 11:43:30 +0900

    Expose jobs module boundary

---
+++ templates/platform/src/modules/jobs/public.ts
@@ -0,0 +1,10 @@
+export {
+  enqueueJob,
+  processQueueBatch,
+  registerJobHandler,
+  type EnqueueJobInput,
+  type JobHandler,
+  type JobMessage,
+  type QueueBatchLike,
+  type QueueMessageLike,
+} from './service';
